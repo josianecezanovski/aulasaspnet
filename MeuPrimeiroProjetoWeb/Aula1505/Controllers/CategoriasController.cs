3b6fa14e2e7caa075ccd20cf0e740caf2951a258
Josiane <josianecezanovski@outlook.com>
2018-05-15 20:06:25 -0300

    Novo Projeto , Criando Categoria e view

---
+++ MeuPrimeiroProjetoWeb/Aula1505/Controllers/CategoriasController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aula1505.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        // GET: Categorias
+        public ActionResult Index()
+        {
+            List<string> categorias = new List<string>();
+            categorias.Add("Estudo");
+            categorias.Add("Lazer");
+            categorias.Add("Refeiçao");
+            categorias.Add("Trabalo");
+
+            ViewBag.MinhasCategorias = categorias;
+
+
+
+            return View();
+        }
+    }
+}
---
+++ MeuPrimeiroProjetoWeb/SQLQuery1.sql
@@ -0,0 +1,5 @@
+﻿
+RESTORE DATABASE ADWORKSDB
+FROM DISK = 'C:\Users\Josiane\Downloads\AdventureWorks2016 (1).bak'
+WITH MOVE 'AdventureWorks2016_Data' TO 'C:\Users\Josiane\Downloads\AdventureWorks2016 (1).mdf',
+MOVE 'AdventureWorks2016_Log' TO 'C:\Users\Josiane\Downloads\AdventureWorks2016 (1).ldf' 
